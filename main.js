663d12663558cc6e9937c872b8034effd4e723f8
Across_Coding <105272087+ChaanBiz@users.noreply.github.com>
2023-09-13 04:51:08 +0800

    oz & variant choices updated

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
-const product = document.getElementById("product"),
-show = document.getElementById("show_buy")
+const oz = [
+  "40oz",
+  "32oz",
+  "22oz",
+  "18oz",
+  "14oz"
+]
 
-function show_buy() {
-  
-}
+const variant = [
+  "ST Amethyst",
+  "ST Hunter",
+  "ST Malibu",
+  "ST Moss Green",
+  "ST Stone Gray",
+  "Emerald",
+  "Obsidian",
+  "Pearl",
+  "Rose Quartz",
+  "Ruby",
+  "Sapphire",
+  "Tarzanite",
+  "Topaz"
+]
+
+const ozlist = document.getElementById("oz"),
+varlist = document.getElementById("variant")
+
+
+oz.forEach((item) => {
+  const list = document.createElement("div")
+  list.innerHTML = item;
+  ozlist.appendChild(list)
+})
+
+variant.forEach((item) => {
+  const list = document.createElement("div")
+  list.innerHTML = item;
+  varlist.appendChild(list)
+})
